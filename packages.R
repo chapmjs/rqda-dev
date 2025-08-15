2ea5f02352369c22588ddb6436cdff3bf1aa7e8b
John Chapman <john.chapman@systemsresearch.linkpc.net>
2025-08-14 19:13:35 -0600

    Create packages.R

---
+++ packages.R
@@ -0,0 +1,18 @@
+# packages.R - Install required packages
+install.packages(c(
+  "shiny",
+  "shinydashboard", 
+  "DT",
+  "RMySQL",
+  "DBI",
+  "pool",
+  "shinyjs",
+  "shinyWidgets",
+  "shinycssloaders",
+  "stringr",
+  "dplyr",
+  "plotly",
+  "digest",
+  "glue",
+  "htmltools"
+))
